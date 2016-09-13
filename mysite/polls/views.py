26015f61e4b53c4954c056da01cf84cce00c04f4
Vishak KC <Vishak KC>
2016-09-13 12:18:34 +0530

    Upto Tut 3 initial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,8 +13,8 @@ def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question"
+    response = "You're looking at the results of question "
     return HttpResponse(response + str(question_id)+ '.')
 
 def vote(request, question_id):
-    return HttpResponse('You\'re voting on question' + str(question_id)+'.')
+    return HttpResponse('You\'re voting on question ' + str(question_id)+'.')
